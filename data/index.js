e976728ba8a517e6360577131d313c49fdf17bf5
Samantha Paula <167320236+SammyStos@users.noreply.github.com>
2024-05-30 10:04:20 -0300

    Update index.js

--- data/index.js
+++ data/index.js
@@ -2,38 +2,38 @@ let DATA =  [
     {
     id:'01',
     porcentagem :'5%', 
-    titulo : 'Leitura',
-    imagem :'https://img.freepik.com/vetores-gratis/pilha-de-livros-de-design-plano-desenhado-a-mao_23-2149334862.jpg',
-     Descricao :'vLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever sinic'  
+    titulo : 'Assistir todas as lives das noitadas',
+    imagem :'https://i.pinimg.com/originals/fc/4d/2c/fc4d2cc9cf366bd65ebd09e664cb476f.jpg',
+     Descricao :'Amo assistir o Alan, Cap, Galagol, Fall, Vx, Dino, jogando jogos duvidosos e engraçados'  
     },
     {
      id:'02',
      porcentagem :'50%', 
-    titulo : 'Trabalho',
-     imagem :'https://www.incqs.fiocruz.br/images/stories/incqs/Relaes-trabalho-1.jpg',
-     Descricao :'vLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsumt has survived not only five centuries, but also the leap into electronic' 
+    titulo : 'Amo jogar The Sims 4',
+     imagem :'https://image.api.playstation.com/vulcan/img/cfn/11307Icil4NJY5ns0ctrCU6yIDMc34VUBKxBc1bSouc4XDDIPQMyn6D78dVkjbPopiaRUUsHS7NdKjQDOQt2rxm4ATAt5nu3.png',
+     Descricao :'Amo criar historias nesse jogo, se deixar fico mais de 5 horas só jogando é muito viciante' 
     },
     {
         id:'03',
         porcentagem :'25%', 
-        titulo : 'Descanso',
-        imagem :'https://s1.abcstatics.com/abc/www/multimedia/economia/2023/02/10/sleep-gcc-Rb0Ia3m6tQ1Y32GLy6i2clK-1200x840@abc.png',
-        Descricao :'vLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsumt has survived not only five centuries, but also the leap into electronic'  
+        titulo : 'Ler ou assistir Mangás/Animes',
+        imagem :'https://wallpapercave.com/wp/wp12527931.jpg',
+        Descricao :'Sempre que conheço algum mangá ou anime, eu fico viciada e leio/assisto no mesmo dia(amo aqueles de reencarnação)'  
     },
     {
         id:'04',
         porcentagem :'5%', 
-        titulo : 'academia',
-        imagem :'https://cdn.pixabay.com/photo/2024/03/09/09/55/ai-generated-8622357_640.jpg',
-        Descricao :'vLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsumt has survived not only five centuries, but also the leap into electronic'  
+        titulo : 'Dormir',
+        imagem :'https://i.pinimg.com/originals/1d/a3/df/1da3df1dd211a2770f7b95e57978ecaf.jpg',
+        Descricao :'Amo dormir, ainda mais nesse friozinho'  
     },
 
     {
         id:'05',
         porcentagem :'5%', 
-        titulo : 'Entretenimento',
-        imagem :'https://static.vecteezy.com/ti/vetor-gratis/p3/34790857-entretenimento-controle-desenho-animado-ilustracao-vetor.jpg',
-        Descricao :'vLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsumt has survived not only five centuries, but also the leap into electronic'  
+        titulo : 'Comer Comida',
+        imagem :'https://manualdohomemmoderno.com.br/files/2020/05/como-fazer-o-melhor-hamburguer-caseiro-como-escolher-as-carnes-e-2-receitas-simples-e-praticas-como-fazer-o-melhor-hamburguer-caseiro-como-escolher-as-carnes-e-2-receitas-simples-e-praticas-4.jpg',
+        Descricao :'Amo comer qualquer que seja a comida, mais amo principalmente hamburguer'  
     }
 
 
