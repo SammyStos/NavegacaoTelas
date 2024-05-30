9daefbbed7369621b978b71fd0d1ac6f8ffd0891
SammyStos <samanthapaulaa@gmail.com>
2024-05-30 09:42:27 -0300

    Navegação de telas react

---
+++ Screens/Details/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, StyleSheet, View, Image,FlatList} from 'react-native';
+
+
+
+export default function Details() {
+  return (
+    <View style={estilo.container}>
+      <Text>PAGINA DE DETALHES</Text>
+      
+      </View>     
+
+    
+  );
+}
+const estilo = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#CAF0F8',
+    borderColor: '#90E0EF',
+    paddingHorizontal: 20,
+  }
+});
